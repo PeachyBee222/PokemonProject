ee9a1ef80e6fb7b3b38ef93d77192a1b01ea0b1c
Gibo-Will02 <73460098+Gibo-Will02@users.noreply.github.com>
2024-04-03 13:44:27 +0000

    Modified database designs to reflect database design

--- Database/Pokemon-Database.sql
+++ Database/Pokemon-Database.sql
@@ -43,8 +43,8 @@ CREATE TABLE Pokemon.Creatures
         CONSTRAINT [NE_Pokemon_Creatures_Defense] CHECK(Defense > 0),
     Speed INT NOT NULL
         CONSTRAINT [NE_Pokemon_Creatures_Speed] CHECK(Speed > 0),
-    Total INT NOT NULL
-        CONSTRAINT [TE_Pokemon_Creatures_Total] CHECK(Total = 0) --Has to be equal to BaseHP + Attack + Defense + Speed
+    Total INT NOT NULL,
+    CONSTRAINT [TE_Pokemon_Creatures_Total] CHECK(Total = (BaseHP + Attack + Defense + Speed)) --Has to be equal to BaseHP + Attack + Defense + Speed
 )
 
 CREATE TABLE Pokemon.CreatureElement
@@ -53,7 +53,7 @@ CREATE TABLE Pokemon.CreatureElement
         REFERENCES Pokemon.Creatures(CreatureID),
     ElementID INT NOT NULL FOREIGN KEY
         REFERENCES Pokemon.Element(ElementID),
-    PRIMARY KEY(CreatureID, ElementID),
+    PRIMARY KEY(CreatureID, ElementID)
 )
 
 CREATE TABLE Pokemon.UserCreature
@@ -64,15 +64,39 @@ CREATE TABLE Pokemon.UserCreature
         REFERENCES Pokemon.Creatures(CreatureID),
     Nickname NVARCHAR(100) NULL,
     
-    CONSTRAINT [UK_Pokemon_UserCreature_UserID_CreatureID] UNIQUE
+    CONSTRAINT [UK_Pokemon_UserCreature_UserID_CreatureID] PRIMARY KEY
    (
       UserID,
       CreatureID
-   ),
-
-    CONSTRAINT [UK_Pokemon_UserCreature_UserID_Nickname] UNIQUE
-   (
-      UserID,
-      Nickname
-   ),
+   )
 )
+
+CREATE UNIQUE NONCLUSTERED INDEX [UFX_Pokemon_UserCreature_UserID_Nickname] ON Pokemon.UserCreature
+(
+    UserID,
+    Nickname
+)
+WHERE Nickname IS NOT NULL
+
+
+-- CREATE TABLE Pokemon.UserCreature
+-- (
+--     UserCreatureID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+--     UserID INT NOT NULL FOREIGN KEY
+--         REFERENCES Pokemon.Users(UserID),
+--     CreatureID INT NOT NULL FOREIGN KEY
+--         REFERENCES Pokemon.Creatures(CreatureID),
+--     Nickname NVARCHAR(100) NULL,
+    
+--     CONSTRAINT [UK_Pokemon_UserCreature_UserID_CreatureID] UNIQUE
+--    (
+--       UserID,
+--       CreatureID
+--    ),
+
+--     CONSTRAINT [UK_Pokemon_UserCreature_UserID_Nickname] UNIQUE
+--    (
+--       UserID,
+--       Nickname
+--    ),
+-- )
