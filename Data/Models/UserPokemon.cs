3c2a9702c56cb0a5b51152aa8098ef21ceed42fa
Naomi Harrison <nkh222@ksu.edu>
2024-04-04 18:10:47 -0500

    updates

--- Data/Models/UserPokemon.cs
+++ Data/Models/UserPokemon.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace TheFlyingSaucer.Data.Models
 {
+    /// <summary>
+    /// The pokemon for each user
+    /// </summary>
     public class UserPokemon
     {
+        /// <summary>
+        /// The id of the pokemon
+        /// </summary>
         public int PokemonID { get; set; }
+
+        /// <summary>
+        /// The nickname the user gives the specified pokemon
+        /// </summary>
         public string NickName { get; set; }
 
+        /// <summary>
+        /// The constructor for a user adding a pokemon
+        /// </summary>
+        /// <param name="pokemonID"></param>
+        /// <param name="nickname"></param>
         public UserPokemon(int pokemonID, string? nickname)
         {
             PokemonID = pokemonID;
