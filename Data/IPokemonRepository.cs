79d18a4fb1ce79e5e365659ca0e3aee15a9a6b28
Larien27 <lucie.beke@gmail.com>
2024-04-18 16:46:58 -0500

    commented pokemon repository

--- Data/IPokemonRepository.cs
+++ Data/IPokemonRepository.cs
@@ -4,13 +4,58 @@ namespace TheFlyingSaucer.Data
 {
     public interface IPokemonRepository
     {
+        /// <summary>
+        /// gets all of the pokemon from the database
+        /// </summary>
+        /// <returns>a list of all pokemon</returns>
         List<Pokemon> RetrievePokemons();
+        /// <summary>
+        /// gets all of the users from the database
+        /// </summary>
+        /// <returns>a list of all users</returns>
         List<User> RetrieveUsers();
+        /// <summary>
+        /// creates a new pokemon
+        /// </summary>
+        /// <param name="generationNum">generation number of the pokemon</param>
+        /// <param name="name">name of the pokemon</param>
+        /// <param name="baseHP">base HP of the pokemon</param>
+        /// <param name="attack">attack of the pokemon</param>
+        /// <param name="defense">defense of the pokemon</param>
+        /// <param name="speed">speed of the pokemon</param>
+        /// <param name="pelem">first element type of the pokemon</param>
+        /// <param name="selem">second element type of the pokemon</param>
+        /// <param name="numUsers">number of users of the pokemon</param>
+        /// <returns>the new Pokemon object</returns>
         Pokemon CreatePokemon(int generationNum, string name, int baseHP, int attack, int defense, int speed, ElementType pelem, ElementType selem, int numUsers);
+        /// <summary>
+        /// creates a new user
+        /// </summary>
+        /// <param name="email">email of the user</param>
         void CreateUser(string email);
+        /// <summary>
+        /// fetches the pokemon with the specified name
+        /// </summary>
+        /// <param name="name">name of a pokemon</param>
+        /// <returns>found Pokemon object</returns>
         Pokemon FetchPokemon(string name);
+        /// <summary>
+        /// gets the pokemon with the specified name
+        /// </summary>
+        /// <param name="name">name of a pokemon</param>
+        /// <returns>found Pokemon object</returns>
         Pokemon GetPokemon(string name);
+        /// <summary>
+        /// gets the user with the specified email
+        /// </summary>
+        /// <param name="email">email of the user</param>
+        /// <returns>found User object</returns>
         User GetUser(string email);
+        /// <summary>
+        /// adds the pokemon to the user
+        /// </summary>
+        /// <param name="pokemon">Pokemon object</param>
+        /// <param name="nickname">Pokemon nickname</param>
         void AddPokemon(Pokemon pokemon, string nickname);
     }
 }
--- Data/SqlPokemonRepository.cs
+++ Data/SqlPokemonRepository.cs
@@ -12,7 +12,20 @@ namespace TheFlyingSaucer.Data
         {
             executor = new SqlCommandExecutor(connectionString);
         }
-
+        /// <summary>
+        /// creates a new pokemon
+        /// </summary>
+        /// <param name="generationNum">generation number of the pokemon</param>
+        /// <param name="name">name of the pokemon</param>
+        /// <param name="baseHP">base HP of the pokemon</param>
+        /// <param name="attack">attack of the pokemon</param>
+        /// <param name="defense">defense of the pokemon</param>
+        /// <param name="speed">speed of the pokemon</param>
+        /// <param name="pelem">first element type of the pokemon</param>
+        /// <param name="selem">second element type of the pokemon</param>
+        /// <param name="numUsers">number of users of the pokemon</param>
+        /// <returns>the new Pokemon object</returns>
+        /// <exception cref="ArgumentException"></exception>
         public Pokemon CreatePokemon(int generationNum, string name, int baseHP, int attack, int defense, int speed, ElementType pelem, ElementType selem, int numUsers)
         {
             if (string.IsNullOrWhiteSpace(name))
@@ -21,7 +34,11 @@ namespace TheFlyingSaucer.Data
             var d = new CreatePokemonDataDelegate(generationNum, name, baseHP, attack, defense, speed, pelem, selem, numUsers);
             return executor.ExecuteNonQuery(d);
         }
-        
+        /// <summary>
+        /// creates a new user
+        /// </summary>
+        /// <param name="email">email of the user</param>
+        /// <exception cref="ArgumentException"></exception>
         public void CreateUser(string email)
         {
             if (string.IsNullOrWhiteSpace(email))
@@ -30,25 +47,41 @@ namespace TheFlyingSaucer.Data
             var d = new CreateUserDataDelegate(email);
             executor.ExecuteNonQuery(d);
         }
-        
+        /// <summary>
+        /// fetches the pokemon with the specified name
+        /// </summary>
+        /// <param name="name">name of a pokemon</param>
+        /// <returns>found Pokemon object</returns>
         public Pokemon FetchPokemon(string name)
         {
             var d = new FetchPokemonDataDelegate(name);
             return executor.ExecuteReader(d);
         }
-        
+        /// <summary>
+        /// gets the pokemon with the specified name
+        /// </summary>
+        /// <param name="name">name of a pokemon</param>
+        /// <returns>found Pokemon object</returns>
         public Pokemon GetPokemon(string name)
         {
             var d = new GetPokemonDataDelegate(name);
             return executor.ExecuteReader(d);
         }
-        
+        /// <summary>
+        /// gets the user with the specified email
+        /// </summary>
+        /// <param name="email">email of the user</param>
+        /// <returns>found User object</returns>
         public User GetUser(string email)
         {
             var d = new GetUserDataDelegate(email);
             return executor.ExecuteReader(d);
         }
-
+        /// <summary>
+        /// adds the pokemon to the user
+        /// </summary>
+        /// <param name="pokemon">Pokemon object</param>
+        /// <param name="nickname">Pokemon nickname</param>
         public void AddPokemon(Pokemon pokemon, string nickname)
         {
             var d = new AddPokemonDataDelegate(pokemon, nickname);
@@ -62,6 +95,10 @@ namespace TheFlyingSaucer.Data
         {
             return executor.ExecuteReader(new RetrievePokemonDataDelegate());
         }
+        /// <summary>
+        /// gets all of the users from the database
+        /// </summary>
+        /// <returns>a list of all users</returns>
         public List<User> RetrieveUsers()
         {
             return executor.ExecuteReader(new RetrieveUserDataDelegate());
